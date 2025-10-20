aad70ea5a0e268cc1d541348fa306125035eac2d
sendosend <johnsenberdin2930@gmail.com>
2025-10-21 01:02:59 +0800

    updated

--- project-1/src/components/Footer.jsx
+++ project-1/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-6 bg-slate-800 border-t border-slate-800 shadow-md">
       <p className="text-center text-sm">
-        © 2024 Berdin development. All rights reserved.
+        © 2025 Berdin development. All rights reserved.
       </p>
     </footer>
   );
