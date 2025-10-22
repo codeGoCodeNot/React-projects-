7f17d5b9bbe872e3b3d10b67db3c70cc07c0bf56
sendosend <johnsenberdin2930@gmail.com>
2025-10-22 20:18:55 +0800

    responsive tailwind css and react

---
+++ project-3/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { FaGlobe } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="bg-[#F55A5A] w-full">
+      <div
+        className="
+      font-semibold text-[#FFFFFF] 
+      flex justify-center items-center  
+      py-4 md:p-6 lg:p-10 xl:p-12 2xl:p-14
+      gap-2 lg:gap-4 2xl:gap-5
+      text-lg md:text-xl xl:text-2xl 2xl:text-3xl
+      "
+      >
+        <FaGlobe
+          className="
+        text-2xl
+        md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl
+        "
+        />
+        <h1>my travel journal.</h1>
+      </div>
+    </header>
+  );
+};
+export default Header;
