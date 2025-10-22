02ecd41dbc613559082e286c6c5e2b62bbe356e1
sendosend <johnsenberdin2930@gmail.com>
2025-10-23 03:05:46 +0800

    editing image src for netlify

--- project-3/src/components/data.js
+++ project-3/src/components/data.js
@@ -7,7 +7,7 @@ const travelData = [
     endDate: "24 Jan, 2023",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    image: "./public/japan.png",
+    image: "/japan.png",
     alt: "Mount Fuji mountain in Japan with snow capped peak",
   },
   {
@@ -18,7 +18,7 @@ const travelData = [
     endDate: "8 Jun, 2023",
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century’s most famous and distinctive buildings.",
-    image: "./public/sydney.png",
+    image: "/sydney.png",
     alt: "Sydney Opera House and Sydney Harbour Bridge in Australia",
   },
   {
@@ -29,7 +29,7 @@ const travelData = [
     endDate: "18 Nov, 2024",
     description:
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    image: "./public/norway.png",
+    image: "/norway.png",
     alt: "Geirangerfjord fjord in Norway with scenic water and mountains",
   },
 ];
